edbb7fe57144b9980122fcbaea120c6a0c290fba
Dannylo Cardoso Maurício <danmauricio001@gmail.com>
2026-03-14 20:43:50 -0300

    add: angle and quadangle structs

---
+++ src/scanner/Scanner/Scanner.Core/Models/Angle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Scanner.Core.Models
+{
+    public readonly struct Angle
+    {
+        double Radians { get; }
+
+        private Angle(double radians)
+        {
+            Radians = radians;
+        }
+
+        public static Angle FromPoints(Point[] a, Point[] b, Point[] c)
+        {
+            // todo: implement cosine formula for vectors
+            return new Angle(0);
+        }
+    }
+
+    public interface IPolygonAngles { int VertexCount { get; } }
+    public readonly record struct QuadrilateralAngles
+        (
+        double TopLeft,
+        double TopRight,
+        double BottomRight,
+        double BottomLeft
+        ) : IPolygonAngles
+    {
+        public int VertexCount => 4;
+        
+        public static QuadrilateralAngles FromQuadrilateral(Quadrilateral quad)
+        {
+            // todo: call angle struct to fill in the angles
+            return new QuadrilateralAngles(0, 0, 0, 0);
+        }
+    }
+}
--- src/scanner/Scanner/Scanner.Core/obj/Debug/net10.0/Scanner.Core.AssemblyInfo.cs
+++ src/scanner/Scanner/Scanner.Core/obj/Debug/net10.0/Scanner.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Scanner.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec033f79062efb183e324dfddbf3fd9f971f73cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a79fbfbaac025068361ca1e5dd4dc86e716699c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Scanner.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Scanner.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
