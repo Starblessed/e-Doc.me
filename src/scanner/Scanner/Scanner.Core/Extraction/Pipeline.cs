abccc708d8891d0c70885e71033931e5ff82dd81
Dannylo Cardoso Maurício <danmauricio001@gmail.com>
2026-03-16 17:37:59 -0300

    add: pipeline methods

--- src/scanner/Scanner/Scanner.Core/Extraction/Pipeline.cs
+++ src/scanner/Scanner/Scanner.Core/Extraction/Pipeline.cs
@@ -2,9 +2,39 @@
 using System.Collections.Generic;
 using System.Text;
 
+using OpenCvSharp;
+using Scanner.Core.Models;
+using Scanner.Core.Extraction;
+
 namespace Scanner.Core.Extraction
 {
-    internal class Pipeline
+    public static class Pipeline
     {
+        private static Mat LoadImage(string path)
+        {
+            // todo: load an image into memory
+            return new Mat();
+        }
+
+        private static void SaveImage(string path, Mat image)
+        {
+            // todo: save an image to a path
+        }
+
+        public static string AutoExtract(string imagePath, string saveToPath)
+        {
+            Mat image = LoadImage(imagePath);
+            Quadrilateral? documentContour = AutoExtractor.AutoExtract(image);
+
+            if (documentContour == null)
+            {
+                throw new ArgumentNullException(nameof(documentContour),"Could not find any documents in the image.");
+            }
+
+            SaveImage(saveToPath, image);
+
+            return saveToPath;
+            
+        }
     }
 }
